e5b2630fb6b6907a7fc038b750a82e3f2b8c2600
Derek Hockenberry <dhocken@claws-compute3.ece.ncsu.edu>
2025-09-04 17:44:37 -0400

    setup.sh w new mods, vm datasheet

--- Analog/cc/setup.sh
+++ Analog/cc/setup.sh
@@ -1,4 +1,5 @@
 export PDK_DIR=/mnt/designkits/gf180MCU/dk_ncsu/gf180mcu
+export EDITOR=vi	# DH, 09/04/25
 
 if [ ! -f ${PWD}/lib.defs ]; then
   cp $PDK_DIR/setup/lib.defs .
@@ -6,11 +7,19 @@ fi
 
 # loads versions used for freepdk3
 # TODO find newest version that works for everything (no glibc issues)
-ml syn/W-2024.09-SP2
-ml primesim/W-2024.09-SP1-2
-ml icvalidator/W-2024.09-SP2
-ml customcompiler/W-2024.09-SP1-2
-ml primewave/W-2024.09-SP1-2
+#ml syn/W-2024.09-SP2
+ml syn
+#ml primesim/W-2024.09-SP1-2
+ml primesim
+#ml icvalidator/W-2024.09-SP2
+ml icvalidator
+#ml customcompiler/W-2024.09-SP1-2
+ml customcompiler
+#ml primewave/W-2024.09-SP1-2
+ml primewave
+ml vcs			# DH, 09/04/25
+ml verdi		# DH, 09/04/25
+ml hspice		# DH, 09/04/25
 
 # unsure if needed
 # ml icvalidator/W-2024.09-SP2
